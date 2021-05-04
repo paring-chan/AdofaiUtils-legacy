ec1a48654b4516887ca4197da9ff2b003d3d10b8
파랑이 <parangee9706@gmail.com>
2021-05-04 21:49:27 +0900

    initial commit

---
+++ Main.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using UnityModManagerNet;
+using HarmonyLib;
+
+namespace AdofaiUtils
+{
+    internal static class Main
+    {
+        private static Harmony _harmony;
+        private static UnityModManager.ModEntry _mod;
+
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            _mod = modEntry;
+            _mod.OnToggle = OnToggle;
+
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            _mod = modEntry;
+
+            if (value) StartTweaks();
+            else StopTweaks();
+
+            return true;
+        }
+
+        private static void StartTweaks()
+        {
+            _harmony = new Harmony(_mod.Info.Id);
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        private static void StopTweaks()
+        {
+            _harmony.UnpatchAll(_harmony.Id);
+        }
+    }
+}
---
+++ build.js
@@ -0,0 +1,28 @@
+﻿const fs = require('fs')
+const cp = require('child_process')
+const yargs = require('yargs-parser')
+const rimraf = require('rimraf')
+const archiver = require('archiver')
+const info = require('./Info.json');
+
+(async () => {
+    cp.execSync('dotnet "C:\\Program Files\\dotnet\\sdk\\5.0.200\\MSBuild.dll" /p:Configuration=Release')
+
+    rimraf.sync('Release')
+
+    fs.mkdirSync('Release')
+
+    fs.copyFileSync('./bin/Release/Adofaiutils.dll', './Release/AdofaiUtils.dll')
+
+    fs.copyFileSync('./Info.json', './Release/Info.json')
+
+    const args = yargs(process.argv)
+
+    if (args.release) {
+        const zip = archiver('zip', {})
+        const stream = fs.createWriteStream(require('path').join(__dirname, `AdofaiUtils ${info.Version}.zip`))
+        zip.pipe(stream)
+        zip.directory('Release/', '.')
+        await zip.finalize()
+    }
+})()
