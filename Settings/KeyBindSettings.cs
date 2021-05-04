7f54250cd67017b2a1f6e43651ce4cfdd964c4ea
파랑이 <parangee9706@gmail.com>
2021-05-05 00:49:35 +0900

    feat: key bind settings section

---
+++ Settings/KeyBindSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityModManagerNet;
+
+namespace AdofaiUtils
+{
+    [DrawFields(DrawFieldMask.Public)]
+    public class KeyBindSettings
+    {
+        public bool Reload = true;
+        public bool Workshop = true;
+        public bool EnterMap = true;
+    }
+}
