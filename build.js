4d187904335dd343e839d545cca22d0855698aff
파랑이 <parangee9706@gmail.com>
2021-05-04 22:04:48 +0900

    improve build script

--- build.js
+++ build.js
@@ -3,16 +3,18 @@ const cp = require('child_process')
 const yargs = require('yargs-parser')
 const rimraf = require('rimraf')
 const archiver = require('archiver')
-const info = require('./Info.json');
+const info = require('./Info.json')
+const path = require('path')
+const { findSteamAppById } = require('find-steam-app');
 
 (async () => {
-    cp.execSync('dotnet "C:\\Program Files\\dotnet\\sdk\\5.0.200\\MSBuild.dll" /p:Configuration=Release')
-
     rimraf.sync('Release')
+    
+    cp.execSync('dotnet "C:\\Program Files\\dotnet\\sdk\\5.0.200\\MSBuild.dll" /p:Configuration=Release')
 
     fs.mkdirSync('Release')
 
-    fs.copyFileSync('./bin/Release/Adofaiutils.dll', './Release/AdofaiUtils.dll')
+    fs.copyFileSync(`./bin/Release/${info.Id}.dll`, `./Release/${info.Id}.dll`)
 
     fs.copyFileSync('./Info.json', './Release/Info.json')
 
@@ -20,9 +22,17 @@ const info = require('./Info.json');
 
     if (args.release) {
         const zip = archiver('zip', {})
-        const stream = fs.createWriteStream(require('path').join(__dirname, `AdofaiUtils ${info.Version}.zip`))
+        const stream = fs.createWriteStream(path.join(__dirname, `${info.Id}-${info.Version}.zip`))
         zip.pipe(stream)
         zip.directory('Release/', '.')
         await zip.finalize()
+    } else {
+        const appPath = await findSteamAppById(977950)
+        const modPath = path.join(appPath, 'Mods', info.Id)
+        rimraf.sync(modPath)
+        fs.mkdirSync(modPath)
+        fs.copyFileSync(`Release/${info.Id}.dll`, path.join(modPath, info.Id + '.dll'))
+        fs.copyFileSync('Release/Info.json', path.join(modPath, 'Info.json'))
     }
+    console.log('Successful.')
 })()
