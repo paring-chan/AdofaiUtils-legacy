dd97aba60c7c6e9a43beb57f049aa8414057f610
파랑이 <parangee9706@gmail.com>
2021-05-05 00:44:48 +0900

    feat: enable configuration section

--- Main.cs
+++ Main.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using UnityModManagerNet;
 using HarmonyLib;
+using Mono.Unix.Native;
+using UnityEngine.EventSystems;
 
 namespace AdofaiUtils
 {
@@ -8,13 +10,27 @@ namespace AdofaiUtils
     {
         private static Harmony _harmony;
         private static UnityModManager.ModEntry _mod;
+        private static Settings settings;
 
         private static bool Load(UnityModManager.ModEntry modEntry)
         {
+            settings = Settings.Load<Settings>(modEntry);
             _mod = modEntry;
             _mod.OnToggle = OnToggle;
-            
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+
             return true;
+        }
+
+        private static void OnGUI(UnityModManager.ModEntry entry)
+        {
+            settings.Draw(entry);
+        }
+        
+        private static void OnSaveGUI(UnityModManager.ModEntry entry)
+        {
+            settings.Save(entry);
         }
 
         private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
--- Settings.cs
+++ Settings.cs
@@ -1,7 +1,18 @@
-﻿namespace AdofaiUtils
+﻿using UnityModManagerNet;
+
+namespace AdofaiUtils
 {
-    public class Settings
+    public class Settings : UnityModManager.ModSettings, IDrawable
     {
-        
+        [Draw("Test")] public bool test = true;
+
+        public void OnChange()
+        {
+        }
+
+        public override void Save(UnityModManager.ModEntry entry)
+        {
+            Save(this, entry);
+        }
     }
 }
