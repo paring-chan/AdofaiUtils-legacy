1689fbbad43c096df0d7f7f664860d808800c936
파랑이 <parangee9706@gmail.com>
2021-05-05 00:59:36 +0900

    add sub collapse to key bind settings

--- Settings/KeyBindSettings.cs
+++ Settings/KeyBindSettings.cs
@@ -5,11 +5,17 @@ namespace AdofaiUtils
     [DrawFields(DrawFieldMask.Public)]
     public class KeyBindSettings
     {
-        [Draw("리로드(R)")]
-        public bool Reload = true;
-        [Draw("창작마당 열기(W)")]
-        public bool Workshop = true;
-        [Draw("맵 바로 입장(왼쪽 방향키)")]
-        public bool EnterMap = true;
+        [Draw("CLS", Collapsible = true)] public CLSKeyBindSettings clsKeyBindSettings = new CLSKeyBindSettings();
+        
+        [DrawFields(DrawFieldMask.Public)]
+        public class CLSKeyBindSettings
+        {
+            [Draw("리로드(R)")]
+            public bool Reload = true;
+            [Draw("창작마당 열기(W)")]
+            public bool Workshop = true;
+            [Draw("맵 바로 입장(왼쪽 방향키)")]
+            public bool EnterMap = true;
+        }
     }
 }
