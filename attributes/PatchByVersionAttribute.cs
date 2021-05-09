4d6861f0241a6977198dec32bd4eb80638c56022
파랑이 <parangee9706@gmail.com>
2021-05-09 20:26:49 +0900

    feat: init version patch attribute

---
+++ attributes/PatchByVersionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdofaiUtils.attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class R68OnlyPatch : Attribute
+    {
+    }
+    
+    [AttributeUsage(AttributeTargets.Class)]
+    public class R71OnlyPatch : Attribute
+    {
+    }
+}
