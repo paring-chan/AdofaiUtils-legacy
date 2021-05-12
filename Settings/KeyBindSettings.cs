210794f38dd83fc6543caf3a4c6cb78df6642618
파랑이 <parangee9706@gmail.com>
2021-05-12 19:58:45 +0900

    feat: key bind setting

--- Settings/KeyBindSettings.cs
+++ Settings/KeyBindSettings.cs
@@ -1,4 +1,5 @@
-﻿using UnityModManagerNet;
+﻿using UnityEngine;
+using UnityModManagerNet;
 
 namespace AdofaiUtils.Settings
 {
@@ -8,29 +9,45 @@ namespace AdofaiUtils.Settings
         [DrawFields(DrawFieldMask.Public)]
         public class CLSKeyBindSettings
         {
-            [Draw("리로드(R)")]
-            public bool Reload = true;
-            [Draw("창작마당 열기(W)")]
-            public bool Workshop = true;
-            [Draw("에디터 열기(E)")]
-            public bool Editor = true;
-            [Draw("맵 바로 입장(왼쪽 방향키)")]
-            public bool EnterMap = true;
-            [Draw("맵 정보 보기(I)")]
-            public bool MapInfo = true;
+            [Draw("리로드")]
+            public KeyBindSetting Reload = new KeyBindSetting(KeyCode.R);
+            [Draw("창작마당 열기")]
+            public KeyBindSetting Workshop = new KeyBindSetting(KeyCode.W);
+            [Draw("에디터 열기")]
+            public KeyBindSetting Editor = new KeyBindSetting(KeyCode.E);
+            [Draw("맵 바로 입장")]
+            public KeyBindSetting EnterMap = new KeyBindSetting(KeyCode.LeftArrow);
+            [Draw("맵 정보 보기")]
+            public KeyBindSetting MapInfo = new KeyBindSetting(KeyCode.I);
         }
         
         [DrawFields(DrawFieldMask.Public)]
         public class EditorKeyBindSettings
         {
-            [Draw("에디터 나가기(Ctrl+Q)")]
-            public bool Quit = true;
+            [Draw("에디터 나가기")]
+            public KeyBindSetting Quit = new KeyBindSetting(KeyCode.Q);
         }
         
         public class CustomPlayKeyBindSettings
         {
             [Draw("즉시 재시작(R)")] public bool Restart = true;
         }
+    }
+
+    public class KeyBindSetting
+    {
+        private KeyCode _keyCode;
+        
+        public KeyBindSetting(KeyCode keyCode, bool defaultEnabled = true)
+        {
+            _keyCode = keyCode;
+            Key = new KeyBinding {keyCode = keyCode};
+            Enabled = defaultEnabled;
+        }
+        
+        [Draw("활성화")] public bool Enabled;
+
+        [Draw("키 설정")] public KeyBinding Key;
     }
     
     [DrawFields(DrawFieldMask.Public)]
